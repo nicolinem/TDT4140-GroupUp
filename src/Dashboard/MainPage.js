ff3f4a1a006fac6a1df81e6ea32e5c356f01fd14
Nicoline Mork <nicolimo@stud.ntnu>
2022-02-15 15:29:19 +0100

    #10 feat: implement routing The main page now displays other components inside that changes with routing

--- src/Dashboard/MainPage.js
+++ src/Dashboard/MainPage.js
@@ -6,37 +6,30 @@ import { sizing } from '@mui/system';
 
 import React from 'react';
 import { Sidebar } from './Sidebar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Outlet,
+
+} from "react-router-dom";
+import { Feed } from './Feed';
 
 export const  MainPage = () => {
 
   return (
     <Box sx={{ flexGrow: 1, }}>
       <Header />
+      
       <Box sx={{ display: 'flex', minWidth: 140}}>
       
-      <Box sx={{ display: 'flex', minWidth: 250, mt: 5, ml: 3}}>
-        <Sidebar />
-      </Box>
-
-      <Box sx={{ px:5, py: 4 }}>
-      <Grid container spacing={1}>
-       
-
-          <Grid item sm={3}>
-            <GroupCard />
-          </Grid>
-          <Grid item xs={3}>
-            <GroupCard />
-          </Grid>
-          <Grid item sm={3}>
-            <GroupCard />
-          </Grid>
-          <Grid item sm={3}>
-            <GroupCard />
-          </Grid>
-          
-      </Grid>
-      </Box>
+        <Box sx={{ display: 'flex', minWidth: 250, mt: 6, ml: 3}}>
+          <Sidebar />
+        </Box>
+        <Box sx={{ px:5, py: 4 }}>
+          <Outlet />
+        </Box>
       </Box>
      
     </Box>
