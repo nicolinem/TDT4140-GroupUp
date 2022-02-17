76acc841b32780b1d3ce9178d7b64b15d8dedef7
Nicoline Mork <nicolimo@stud.ntnu>
2022-02-17 20:26:11 +0100

    #11 feat: add routing based on auth The main page redirects the user to the login page if not logged in

--- src/Dashboard/MainPage.js
+++ src/Dashboard/MainPage.js
@@ -1,37 +1,33 @@
+import { Box } from "@mui/material";
+import { Header } from "./Header";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-import { Box, Grid, Stack } from '@mui/material';
-import { Header } from './Header';
-import { GroupCard} from './GroupCard';
-import { sizing } from '@mui/system';
+import React from "react";
 
-import React from 'react';
-import { Sidebar } from './Sidebar';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Outlet,
+import { Outlet, useNavigate, } from "react-router-dom";
 
-} from "react-router-dom";
-import { Feed } from './Feed';
 
-export const  MainPage = () => {
+export const MainPage = () => {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log("hello");
+    } else {
+      navigate("/login");
+    }
+  });
 
   return (
-    <Box sx={{ flexGrow: 1, }}>
+    <Box sx={{ flexGrow: 1 }}>
       <Header />
-      
-      <Box sx={{ display: 'flex', minWidth: 140}}>
-      
-        
-        <Box sx={{ px:5, py: 4 }}>
+
+      <Box sx={{ display: "flex", minWidth: 140 }}>
+        <Box sx={{ px: 5, py: 4 }}>
           <Outlet />
         </Box>
       </Box>
-     
     </Box>
-
-      );
-
-}
+  );
+};
