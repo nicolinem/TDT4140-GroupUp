4f0d80f07412ca5c31025e630584cf0c58a5713c
Nicoline Mork <nicolimo@stud.ntnu>
2022-03-15 12:58:22 +0100

    #18 style

--- src/Chat/ChatMessage.js
+++ src/Chat/ChatMessage.js
@@ -30,32 +30,66 @@ export const ChatMessage = (props) => {
     // border: "2px solid #000",
     borderRadius: "10px",
     boxShadow: 24,
-    m: 1,
+    mb: 0.5,
+    p: 1,
+  };
+  const userUserMessage = {
+    display: "inline-flex",
+    alignSelf: "flex-end",
+    bgcolor: "#b3e5cc",
+    // border: "2px solid #000",
+    borderRadius: "10px",
+    boxShadow: 24,
+    mb: 0.5,
     p: 1,
   };
   const externalUserMessage = {
     display: "inline-flex",
     alignSelf: "flex-start",
-    bgcolor: "orange",
+    bgcolor: "#79d2a6",
     // border: "2px solid #000",
     borderRadius: "10px",
     boxShadow: 24,
-    m: 1,
+    mb: 0.5,
     p: 1,
   };
-  const { text, uid, photoURL, userGroupID } = props.message;
+  const { text, uid, photoURL, userGroupID, displayName } = props.message;
   const currentUserGroupID = props.currentUserGroupID;
   const messageType =
-    currentUserGroupID === userGroupID ? userMessage : externalUserMessage;
+    currentUserGroupID != userGroupID
+      ? externalUserMessage
+      : uid === auth.currentUser.uid
+      ? userUserMessage
+      : userMessage;
+  const alignment =
+    currentUserGroupID != userGroupID ? "flex-start" : "flex-end";
+  console.log(alignment);
   // uid === auth.currentUser.uid ? userMessage : externalUserMessage;
   return (
-    <Box sx={messageType}>
-      <img
-        src={
-          photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
-        }
-      />
-      <Typography>{text}</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        m: 0,
+      }}
+    >
+      <Box display="inline-flex" alignSelf={alignment}>
+        <Typography
+          sx={{ m: 0, p: 0, display: "inline-block" }}
+          variant="caption"
+          gutterbottom
+        >
+          {displayName}
+        </Typography>
+      </Box>
+      <Box sx={messageType}>
+        <img
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+        />
+        <Typography>{text}</Typography>
+      </Box>
     </Box>
   );
 };
