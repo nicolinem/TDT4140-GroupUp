d06ad49b9fb6c29ae3acd7573bc84a0b58e61269
Nicoline Mork <nicolimo@stud.ntnu>
2022-03-18 07:09:48 +0100

    export const auth

--- src/firebase.js
+++ src/firebase.js
@@ -30,7 +30,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
-const auth = getAuth();
+export const auth = getAuth();
 export const storage = getStorage();
 export const storageRef = ref(storage);
 
